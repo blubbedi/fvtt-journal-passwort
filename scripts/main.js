476e8be2f26a508cfbf28dba6aef017db72db83b
blubbedi <manueloehlers@web.de>
2025-05-02 15:28:40 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+
+Hooks.once("ready", () => {
+  if (!game.user.isGM) return;
+
+  game.socketlib.register("passwortJournalFreigabe", async ({ playerId, journalName }) => {
+    const journal = game.journal.getName(journalName);
+    if (!journal) {
+      ui.notifications.warn(`Journal „${journalName}“ nicht gefunden.`);
+      return;
+    }
+
+    const permissions = foundry.utils.duplicate(journal.permission);
+    permissions[playerId] = CONST.DOCUMENT_PERMISSION_LEVELS.OBSERVER;
+    await journal.update({ permission: permissions });
+  });
+});
